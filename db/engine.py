640465b012e794b06c3cdc31eb1d3e8941e71ec9
fanni <fmalevych@zoolatech.com>
2025-03-19 11:21:09 +0200

    set new database url

--- db/engine.py
+++ db/engine.py
@@ -1,11 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DB_URL = "sqlite:///./notes.db"
+load_dotenv()
+
+SQLALCHEMY_DB_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DB_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DB_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
